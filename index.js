57db609e534e8d5b2fafdf088d7a840c914d54b8
Taylor Williams <taylor@thewilliams.net>
2019-02-06 05:59:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ class Meal{
 class Delivery{
   constructor(mealId, neighborhoodId, customerId){
     this.mealId = mealId
-    this.neighborhoodCountId = neighborhoodCountId
+    this.neighborhoodId = neighborhoodId
     this.customerId = customerId
     this.id = deliveryCountId++
     store.deliveries.push(this)
