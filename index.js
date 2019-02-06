852eb232adaf68a463b7cf3674064d5ec7516eac
Taylor Williams <taylor@thewilliams.net>
2019-02-06 05:29:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+let neighborhoodCount = 0
+let customerCount = 0
+let mealCount = 0
+let deliveryCount = 0
+class Neighborhood{
+  constructor(name){
+    this.name = name
+  }
+}
+class Delivery{
+  constructor(mealId, neighborhoodId, customerId){
+    
+  }
+}
