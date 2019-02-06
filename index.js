ba3caa8b7629f699ff034b33746d9bcc82cde9f8
Taylor Williams <taylor@thewilliams.net>
2019-02-06 06:11:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -55,9 +55,7 @@ class Meal{
     return store.deliveries.filter(delivery => delivery.mealId === this.id)
   }
   customers(){
-    allMeals = this.deliveries().map(delivery =>{
-      return delivery.customerId
-    })
+    allMeals = this.deliveries().map(delivery => delivery.customer())
     return [... new set (allMeals)]
   }
   static byprice(){
